a8267eef56e128df537800f876e3ba4e90d567b8
JanaAhmedHussien <jana.ahmedhussein244@gmail.com>
2026-03-25 21:21:26 +0200

    change in the model parameter to make it fail

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ with mlflow.start_run() as run:
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    model = RandomForestClassifier(n_estimators=100)
+    model = RandomForestClassifier(n_estimators=1)
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
